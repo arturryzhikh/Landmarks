73af7117ef73446a54e0f92681e57035dd4a7903
Artur <ryzhiha@gmail.com>
2021-02-08 13:34:50 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Artur Ryzhikh on 08.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
